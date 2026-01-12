99e6c8af8f561bfb7a26e1b56c366a8c979a1048
Sitthan181 <68070181@kmitl.ac.th>
2026-01-12 14:36:49 +0700

    Add lab06
    Add lab06 (next week is final week before midterm [I'm cooked])

---
+++ src/lab06/Employee.java
@@ -0,0 +1,54 @@
+
+public class Employee {
+    private static String nationality = "Thai";
+    private String name;
+    private Wallet wallet;
+    private int energy;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setWallet(Wallet wallet){
+        this.wallet = wallet;
+    }
+    public Wallet getWallet(){
+        return wallet;
+    }
+    public void setEnergy(int energy){
+        this.energy = energy;
+    }
+    public int getEnergy(){
+        return energy;
+    }
+    public static void setNationality(String nationality){
+        Employee.nationality = nationality;
+    }
+    public static String getNationality(){
+        return nationality;
+    }
+    public void eat(Food f){
+        this.energy += Food.getEnergy();
+    }
+    public boolean buyFood(Seller s){
+        Food f = s.sell(this);
+        if (f != null){
+            this.eat(f);
+            return true;
+        }
+        return false;
+    }
+    public boolean equals(Employee e){
+        return this.name.equals(e.getName());
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        return "My name is " + name + ".\nI have " + energy + " energy left.\nI have a balance of " + wallet.getBalance() + " baht.";
+    }
+}
