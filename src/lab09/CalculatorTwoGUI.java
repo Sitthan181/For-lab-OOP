a90b805bd395e283ec8a794bfacead4df0cf1b83
Sitthan181 <68070181@kmitl.ac.th>
2026-02-06 11:00:41 +0700

    fin the lab09
    fin lab 09 no1

--- src/lab09/CalculatorTwoGUI.java
+++ src/lab09/CalculatorTwoGUI.java
@@ -1,11 +1,14 @@
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
-public class CalculatorTwoGUI {
+public class CalculatorTwoGUI implements ActionListener {
     private JFrame fr;
     private JTextField txt;
     private JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9;
     private JButton bPlus, bMinus, bMul, bDiv, bEqual, bClear;
     private JPanel p;
+    private double num1, num2, result;
+    private String operator;
     public CalculatorTwoGUI(){
         fr = new JFrame("kurnkedlekbut9grid");
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,6 +38,24 @@ public class CalculatorTwoGUI {
         bEqual = new JButton("=");
         bDiv = new JButton("/");
         
+        //tam hai button kod dai
+        b0.addActionListener(this);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+        b4.addActionListener(this);
+        b5.addActionListener(this);
+        b6.addActionListener(this);
+        b7.addActionListener(this);
+        b8.addActionListener(this);
+        b9.addActionListener(this);
+        bPlus.addActionListener(this);
+        bMinus.addActionListener(this);
+        bMul.addActionListener(this);
+        bDiv.addActionListener(this);
+        bEqual.addActionListener(this);
+        bClear.addActionListener(this);
+        
         p.add(b7);
         p.add(b8);
         p.add(b9);
@@ -57,5 +78,36 @@ public class CalculatorTwoGUI {
         
         fr.pack();
         fr.setVisible(true);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String command = ae.getActionCommand();
+
+        if (command.matches("[0-9]")) {
+            txt.setText(txt.getText() + command);
+        } else if (command.equals("C")) {
+            txt.setText("");
+            num1 = 0;
+            operator = "";
+        } else if (command.equals("=")) {
+            num2 = Double.parseDouble(txt.getText());
+            if (operator.equals("+")) {
+                result = num1 + num2;
+            } else if (operator.equals("-")) {
+                result = num1 - num2;
+            } else if (operator.equals("x")) {
+                result = num1 * num2;
+            } else if (operator.equals("/")) {
+                if (num2 != 0) {
+                    result = num1 / num2;
+                }
+            }
+            txt.setText(String.valueOf(result));
+        } else {
+            num1 = Double.parseDouble(txt.getText());
+            operator = command;
+            txt.setText("");
+        }
     }
 }
